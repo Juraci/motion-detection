b77206b02ceb8b2fd7af6fca6f22f93b39426aa2
Juraci Vieira <juraci.vieira@gmail.com>
2017-03-10 11:02:33 -0300

    updating motion detection

--- motion-detection.ino
+++ motion-detection.ino
@@ -15,25 +15,36 @@ const char* ssid = "----";
 const char* password = "----";
 const char* endpoint = "https://nodemcu-listener.herokuapp.com/motion";
 const char* httpsFingerPrint = "F5 69 8C CA 29 68 5E 47 26 38 C5 1A 18 F1 8A 6A EA 60 56 4D";
+const int pinOut = D0;
+const int pinIn = D7;
 
 void setup() {
   USE_SERIAL.begin(115200);
-
+  pinMode(pinOut, OUTPUT);
+  pinMode(pinIn, INPUT);
   waitUntilConnection();
 }
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
-    USE_SERIAL.print("connected!\n");
 
-    post();
+    if(digitalRead(pinIn) == HIGH) {
+      digitalWrite(pinOut, HIGH);
+      USE_SERIAL.print("Motion started!\n");
+      post();
+      while(digitalRead(pinIn) == HIGH) {
+        delay(500);
+      }
+      digitalWrite(pinOut, LOW);
+      USE_SERIAL.print("Motion ended!\n");
+    }
 
   } else {
     USE_SERIAL.println("Connection lost");
     waitUntilConnection();
   }
 
-  delay(10000);
+  delay(1000); 
 }
 
 void waitUntilConnection() {
@@ -47,7 +58,7 @@ void waitUntilConnection() {
 
 void post() {
   HTTPClient http;
-
+  
   http.begin(endpoint, httpsFingerPrint);
   http.addHeader("Content-Type", "text/plain");
 
