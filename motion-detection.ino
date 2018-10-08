8921709e606c7d73493b6e2b437754c7ffb1982f
Juraci Vieira <juraci.vieira@gmail.com>
2018-10-07 22:49:54 -0300

    changing from rest to mqtt

--- motion-detection.ino
+++ motion-detection.ino
@@ -1,37 +1,57 @@
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
 #define USE_SERIAL Serial
 
-const char* ssid = "-----";
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const char* ssid = "------";
 const char* password = "------";
-const char* endpoint = "https://nodemcu-listener.herokuapp.com/cards/cardId/motion"; // the number is the board id
-const char* httpsFingerPrint = "F5 69 8C CA 29 68 5E 47 26 38 C5 1A 18 F1 8A 6A EA 60 56 4D";
-const char* authToken = "----";
+
+const char* mqttServer = "______";
+const int mqttPort = 17152;
+const char* clientId = "------";
+const char* outTopic = "-------";
+const char* inTopic = "-------";
+const char* brokerUser = "-------";
+const char* brokerPassword = "--------";
+const int brokerRetryDelay = 5000;
 
 const int sensorInput = D7;
 const int motionLedOut = D0;
 
 const int mainLoopDelay = 1000;
 const int coolDownDelay = 500;
-const int wifiDelay = 1000;
+const int wifiDelay = 500;
 
 void waitUntilConnection(const char* id, const char* pass, int waitTime);
-void post();
+void setupMqtt(const char* server, int port);
+void connectToBroker(const char* id, const char* user, const char* pass, int brokerDelay);
+void callback(char* topic, byte* payload, unsigned int length);
+void mqttPublish();
 void onSensorActivity(void (*callback)());
 
 void setup() {
   USE_SERIAL.begin(115200);
   pinMode(motionLedOut, OUTPUT);
   pinMode(sensorInput, INPUT);
+
   waitUntilConnection(ssid, password, wifiDelay);
+  setupMqtt(mqttServer, mqttPort);
+  connectToBroker(clientId, brokerUser, brokerPassword, brokerRetryDelay);
 }
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
 
-    onSensorActivity(post);
+    if (!client.connected()) {
+      connectToBroker(clientId, brokerUser, brokerPassword, brokerRetryDelay);
+    }
+    client.loop();
+
+    onSensorActivity(mqttPublish);
 
   } else {
     USE_SERIAL.println("Connection lost");
@@ -59,24 +79,53 @@ void onSensorActivity(void (*callback)()) {
 }
 
 void waitUntilConnection(const char* id, const char* pass, int waitTime) {
+  WiFi.mode(WIFI_STA);
   WiFi.begin(id, pass);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(waitTime);
     USE_SERIAL.println("Waiting for connection");
   }
+
+  USE_SERIAL.println("WiFi connected");
 }
 
-void post() {
-  HTTPClient http;
-  
-  http.begin(endpoint, httpsFingerPrint);
-  http.setAuthorization(authToken);
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+void setupMqtt(const char* server, int port) {
+  client.setServer(server, port);
+  client.setCallback(callback);
+}
 
-  int httpCode = http.POST("");
-  USE_SERIAL.println(httpCode);
+void connectToBroker(const char* id, const char* user, const char* pass, int brokerDelay) {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
 
-  http.writeToStream(&Serial);
-  http.end();
+    // Attempt to connect
+    if (client.connect(id, user, pass)) {
+      Serial.println("connected");
+      client.subscribe(inTopic);
+
+    } else {
+
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(brokerDelay);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void mqttPublish() {
+  USE_SERIAL.println("publish");
+  client.publish(outTopic, "motion detected");
 }
